1022f27593d3ef0d5926eb619b5324d229f47418
YuqiYou <you.269@osu.edu>
2022-06-29 20:38:50 -0700

    last

---
+++ C#/Day1/Hw2/part2/PractString/PractString/Program.cs
@@ -0,0 +1,44 @@
+﻿//Question 1 reverse string
+
+/*
+Console.WriteLine("Please enter a String");
+String in1 = Console.ReadLine();
+char[] cArray = in1.ToCharArray();
+String reverse = String.Empty;
+for (int i = cArray.Length - 1; i > -1; i--)
+{
+    reverse += cArray[i];
+}
+
+Console.WriteLine("The reversed array is: " + reverse);
+*/
+
+//Question 2
+//i didnot figure this out...
+
+
+//Question 3
+/*
+String ipt = Console.ReadLine();
+Boolean y = Q3.Pal(ipt);
+
+if (y) { Console.WriteLine("it's a palindrom"); }
+else { Console.WriteLine("not a palindrom"); }
+*/
+
+//Question 4
+
+// Taking a string
+String str = "https://www.apple.com/iphone/";
+
+String[] spearator = { "://, ", "/" };
+Int32 count = 2;
+
+// using the method
+String[] strlist = str.Split(spearator, count,
+       StringSplitOptions.RemoveEmptyEntries);
+
+foreach (String s in strlist)
+{
+    Console.WriteLine(s);
+}
---
+++ C#/Day1/Hw2/part2/PractString/PractString/Q3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Q3
+{
+
+    public static bool Pal(string str)
+    {
+        int end = str.Length - 1;
+
+        for (int i = 0, j = end; i < end; i++, j--)
+        {
+            char frontLetter = str[i];  //Char.ToLower(str[i]);
+            char endLetter = str[j];    //Char.ToLower(str[i]);
+
+            if (frontLetter != endLetter) return false;
+        }
+        return true;
+    }
+
+}
+
